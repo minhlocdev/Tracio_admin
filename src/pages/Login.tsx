88f56eed42f8e84f41e40292117542f07c1ec803
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2025-02-07 01:12:48 +0700

    init login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import Button from "../components/ui/button";
+
+const Login = () => {
+    return ( 
+        <div>
+            <h1>Login</h1>
+            <Button>Login</Button>
+        </div>
+     );
+}
+ 
+export default Login;
