8dee25a6b788abc00afb46a6dfd54238d717da6b
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2025-05-04 00:29:27 +0700

    fix login.ts

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,14 +2,9 @@ import React from "react";
 import LoginForm from "./LoginForm";
 
 const Login: React.FC = () => {
-  const onSubmit = (data: { email: string; password: string }) => {
-    console.log(data);
-    // Handle login logic here (e.g., API call)
-  };
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-200">
-      <LoginForm onSubmit={onSubmit} />
+      <LoginForm />
     </div>
   );
 };
